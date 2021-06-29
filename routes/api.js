b5fe32934c1717c792f2ab4472a090dbdda28d55
unknown <binderyoni@example.com>
2021-06-29 16:47:12 +0300

    improved checkout

--- routes/api.js
+++ routes/api.js
@@ -84,6 +84,33 @@ router.get('/mails', function (req, res, next) {
     res.send('gettting mail route');
 });
 
+router.post('/sendMailToClient', function (req, res, next) {
+
+    const to = req.body.to
+    const subject = req.body.subject
+    const orderNumber=req.body.orderNumber
+    const mail = {
+        from: process.env.THE_MAIL,
+        to: to,
+        subject,
+        text:`
+        Your order no. ${orderNumber} is processed. We will notify you when the order is ready
+        `
+    }
+    transporter.sendMail(mail, (err, data) => {
+        if (err)
+            res.json({
+                status: 'failed',
+                message: err.message
+            })
+        else
+            res.json({
+                status: 'success',
+                reference: req.body.orderNumber
+            })
+    })
+
+});
 
 router.post('/sendMailFromClient', function (req, res, next) {
 
